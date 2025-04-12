7e29d61067bce46f059a0fcef0882d4cf8059b56
PuffCab <berlinkollner@gmail.com>
2025-04-12 18:40:57 +0200

    fixing deployment error extracting page with useSearchParams

--- text-selection/src/app/chatbot/page.tsx
+++ text-selection/src/app/chatbot/page.tsx
@@ -2,27 +2,34 @@
 import { useSearchParams } from "next/navigation";
 import React, { Suspense } from "react";
 
-function Chatbot() {
+function Prompt() {
   const params = useSearchParams();
   const text = params.get("text");
+
   return (
-    <Suspense>
-      <div>
-        <h1>This would be the chatbot page</h1>
-        <p>The prompt could be something like:</p>
-        <p>
-          <i>
-            Give the translation to japanese, ancient greek and esperanto, as
-            well as examples of use using the context provided of the following
-            text:{" "}
-          </i>
-        </p>
-        <p>
-          <strong>{text}</strong>
-        </p>
-      </div>
-    </Suspense>
+    <div>
+      <p>The prompt could be something like:</p>
+      <p>
+        <i>
+          Give the translation to japanese, ancient greek and esperanto, as well
+          as examples of use using the context provided of the following text:{" "}
+        </i>
+      </p>
+      <p>
+        <strong>{text}</strong>
+      </p>
+    </div>
   );
 }
 
+function Chatbot() {
+  return (
+    <div>
+      <h1>This would be the chatbot page</h1>
+      <Suspense>
+        <Prompt />
+      </Suspense>
+    </div>
+  );
+}
 export default Chatbot;
